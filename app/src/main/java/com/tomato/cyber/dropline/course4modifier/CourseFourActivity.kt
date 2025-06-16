a2d619a3cbe27712842cac663285257cabe1346d
yuqiaodan <yuqiaodan@testbird.com>
2025-06-16 18:46:44 +0800

    Modifier

--- app/src/main/java/com/tomato/cyber/dropline/course4modifier/CourseFourActivity.kt
+++ app/src/main/java/com/tomato/cyber/dropline/course4modifier/CourseFourActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.unit.dp
+import com.tomato.cyber.log
 import kotlin.math.min
 
 
@@ -28,11 +38,95 @@ class CourseFourActivity : ComponentActivity() {
                     .safeDrawingPadding()
                     .fillMaxSize()
             ) {
-                LayoutModifierTest2()
+                PointerInputModifierTest()
             }
         }
     }
 }
+
+@Composable
+fun PointerInputModifierTest() {
+
+
+    Box( //可实现：普通点击、长按点击、双击
+        Modifier
+            .pointerInput(Unit) {
+
+                awaitEachGesture{
+
+                }
+
+                awaitPointerEventScope {
+                    val down = awaitFirstDown()
+
+                }
+
+                detectTapGestures(
+                    //普通点击
+                    onTap = {
+                        log("点击")
+                    },
+                    //长按点击
+                    onLongPress = {
+                        log("长按")
+                    },
+                    //双击
+                    onDoubleTap = {
+                        log("双击")
+                    },
+                    //触摸（按下）
+                    onPress = {
+                        log("触摸（按下）")
+                    }
+                )
+            }
+            .size(100.dp)
+            .background(Color.Red))
+
+
+
+  /*  Box( //可实现：普通点击、长按点击、双击
+        Modifier
+            .combinedClickable(
+                //普通点击
+                onClick = {
+                    log("点击")
+                },
+                //长按点击
+                onLongClick = {
+                    log("长按")
+                },
+                //双击
+                onDoubleClick = {
+                    log("双击")
+                }
+            )
+            .size(100.dp)
+            .background(Color.Red))*/
+
+
+}
+
+@Composable
+fun DrawModifierTest() {
+    Text(
+        "yuqiaodan", Modifier
+            .background(Color.Green)
+            .drawWithContent {
+                //右边位置绘制一个圆
+                drawCircle(Color.Red, radius = 5.dp.toPx(), center = Offset(size.width, size.height / 2))
+                //绘制原有的绘制内容 也就是drawWithContent右边的绘制内容 和 组件原本的内容
+                drawContent()
+                //如果不调用 drawContent()，那么组件的绘制内容就不会被绘制 无内容
+                //drawContent()
+                //中间位置绘制一个圆
+                drawCircle(Color.Red, radius = 5.dp.toPx())
+            }
+            //drawWithContent右边的绘制内容会受到drawContent影响
+            .padding(10.dp)
+            .background(Color.Yellow))
+}
+
 
 @Composable
 fun LayoutModifierTest() {
