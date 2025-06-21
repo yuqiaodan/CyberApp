e2c94fa91b734de8ae2a50a022f1e2e890fdc399
yuqiaodan <yuqiaodan@163.com>
2025-06-21 13:45:50 +0800

    complete course4

--- app/src/main/java/com/tomato/cyber/dropline/course4modifier/CourseFourActivity.kt
+++ app/src/main/java/com/tomato/cyber/dropline/course4modifier/CourseFourActivity.kt
@@ -15,7 +15,10 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.LayoutScopeMarker
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,21 +27,45 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.UiComposable
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.LookaheadScope
+import androidx.compose.ui.layout.OnGloballyPositionedModifier
+import androidx.compose.ui.layout.OnPlacedModifier
+import androidx.compose.ui.layout.OnRemeasuredModifier
+import androidx.compose.ui.layout.ParentDataModifier
+import androidx.compose.ui.layout.boundsInParent
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.layout.positionInWindow
+import androidx.compose.ui.modifier.ModifierLocal
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.modifier.modifierLocalProvider
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import com.tomato.cyber.R
 import com.tomato.cyber.log
@@ -59,12 +86,158 @@ class CourseFourActivity : ComponentActivity() {
                     .safeDrawingPadding()
                     .fillMaxSize()
             ) {
-                FrostedGlassEffectExample1()
+
+
             }
         }
     }
 }
 
+@Composable
+fun ModifierLocal(modifier: Modifier = Modifier) {
+
+    val shearWidthKey = modifierLocalOf { "0" }
+    Modifier
+        //提供数据
+        .modifierLocalProvider(shearWidthKey, { "100" })
+        .modifierLocalConsumer {
+            //读取数据
+            shearWidthKey.current
+        }
+
+    Modifier
+        .modifierLocalProvider(shearWidthKey, { "yuqiaodan" })
+        .layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+
+            //怎么把这个witchString局部变量共享到下边的layout去？
+            val widthString = placeable.width.toString()
+
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(0, 0)
+            }
+        }
+        .modifierLocalConsumer {
+            shearWidthKey.current
+        }
+        .layout { measurable, constraints ->
+
+            val placeable = measurable.measure(constraints)
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(0, 0)
+            }
+        }
+
+}
+
+@Composable
+fun PositionedModifierTest(modifier: Modifier = Modifier) {
+
+    Box(
+        Modifier
+            .size(100.dp)
+            .onGloballyPositioned { layoutCoordinates ->
+                layoutCoordinates.localToWindow(Offset.Zero)
+                layoutCoordinates.localToRoot(Offset.Zero)
+
+                layoutCoordinates.positionInParent()
+                layoutCoordinates.positionInWindow()
+                layoutCoordinates.positionInRoot()
+
+                //在父布局中的位置 和 宽高 以RECT的方式返回
+                layoutCoordinates.boundsInParent()
+            })
+
+    Box(Modifier.onPlaced { layoutCoordinates ->
+        layoutCoordinates.localToWindow(Offset.Zero)
+        layoutCoordinates.localToRoot(Offset.Zero)
+
+        layoutCoordinates.positionInParent()
+        layoutCoordinates.positionInWindow()
+        layoutCoordinates.positionInRoot()
+
+        //在父布局中的位置 和 宽高 以RECT的方式返回
+        layoutCoordinates.boundsInParent()
+    })
+}
+
+@Composable
+fun SemanticsModifierTest(modifier: Modifier = Modifier) {
+    Column {
+        Text(text = "床前明月光")
+        Box(
+            Modifier
+                .size(100.dp)
+                .background(Color.Red)
+                //可以清除并重新设置意义
+                .clearAndSetSemantics { }
+                //使用参数mergeDescendants可以合并子组件的semantics 不再被外部组件合并
+                .semantics(mergeDescendants = true) {
+                    //使用contentDescription可以设置组件的意义
+                    contentDescription = "红色方块"
+
+                }) {
+            Text(text = "床前明月光")
+        }
+        //按钮和Text自动合并 contentDescription
+        Button(onClick = {}) {
+            Text(text = "床前明月光")
+        }
+    }
+}
+
+
+@Composable
+fun ParentDataModifierTest(modifier: Modifier = Modifier) {
+    Row {
+        //内部能调用
+        CustomLayout2 {
+            Box(
+                Modifier
+                    .size(40.dp)
+                    .background(Color.Green)
+                    .weightData(1f)
+            )
+        }
+
+    }
+}
+
+
+//使用CustomLayout2Scope修饰content作用域
+@Composable
+fun CustomLayout2(
+    modifier: Modifier = Modifier,
+    content: @Composable @UiComposable CustomLayout2Scope.() -> Unit
+) {
+    Layout(content = { CustomLayout2Scope.content() }, modifier) { measurables, constraints ->
+        measurables.forEach {
+            //此处就能获取到通过weightData设置的weightData
+            val data = it.parentData as? Float
+
+        }
+        layout(100, 100) {
+
+        }
+    }
+}
+
+//使用LayoutScopeMarker注解可以设置weightData使用范围
+@LayoutScopeMarker
+//@Immutable//此注解 减少从组过程中不必要的重组 一般使用在接口上 object不需/要
+object CustomLayout2Scope {
+    fun Modifier.weightData(weight: Float) = then(object : ParentDataModifier {
+        override fun Density.modifyParentData(parentData: Any?): Any? {
+            //parentData 是连续调用的右边的参数
+            return weight
+        }
+    })
+}
+
+
+/**
+ * 高斯模糊测试 - 列表
+ * */
 @Preview
 @Composable
 fun FrostedGlassEffectExample1() {
@@ -85,8 +258,12 @@ fun FrostedGlassEffectExample1() {
                 .hazeSource(hazeState)
         )
 
-        LazyColumn (Modifier.fillMaxSize(), contentPadding = PaddingValues(horizontal = 20.dp), verticalArrangement = Arrangement.spacedBy(12.dp)){
-            items (30){
+        LazyColumn(
+            Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(horizontal = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(30) {
                 Box(
                     modifier = Modifier
                         .align(Alignment.Center)
@@ -97,7 +274,7 @@ fun FrostedGlassEffectExample1() {
                             Color.Transparent
                         )
                         //设置模糊目标（前景）
-                        .hazeEffect(state = hazeState,style = HazeStyle.Unspecified),
+                        .hazeEffect(state = hazeState, style = HazeStyle.Unspecified),
                 ) {
                     // 内容层：负责显示清晰的文字，它没有 blur 修饰符
                     Text(
@@ -112,6 +289,9 @@ fun FrostedGlassEffectExample1() {
     }
 }
 
+/**
+ * 高斯模糊测试 - 单组件
+ * */
 @Preview
 @Composable
 fun FrostedGlassEffectExample() {
@@ -140,7 +320,7 @@ fun FrostedGlassEffectExample() {
                     Color.Black.copy(alpha = 0.4f)
                 )
                 //设置模糊目标（前景）
-                .hazeEffect(state = hazeState,style = HazeStyle.Unspecified),
+                .hazeEffect(state = hazeState, style = HazeStyle.Unspecified),
         ) {
             // 内容层：负责显示清晰的文字，它没有 blur 修饰符
             Text(
@@ -221,7 +401,11 @@ fun DrawModifierTest() {
             .background(Color.Green)
             .drawWithContent {
                 //右边位置绘制一个圆
-                drawCircle(Color.Red, radius = 5.dp.toPx(), center = Offset(size.width, size.height / 2))
+                drawCircle(
+                    Color.Red,
+                    radius = 5.dp.toPx(),
+                    center = Offset(size.width, size.height / 2)
+                )
                 //绘制原有的绘制内容 也就是drawWithContent右边的绘制内容 和 组件原本的内容
                 drawContent()
                 //如果不调用 drawContent()，那么组件的绘制内容就不会被绘制 无内容
